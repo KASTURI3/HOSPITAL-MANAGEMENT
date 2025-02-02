72c7e3e1bf5755afde8a521e182adff2f089f78a
KASTURI3 <202301040278@mitaoe.ac.in>
2025-02-02 21:45:53 +0530

    Completed some frontend work

---
+++ Desktop/frontend/frontend_hos_management/src/app/admindash/admindash.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admindash',
+  standalone: false,
+  
+  templateUrl: './admindash.component.html',
+  styleUrl: './admindash.component.css'
+})
+export class AdmindashComponent {
+
+}
